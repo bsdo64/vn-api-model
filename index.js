0669ab2fd26ff84e36624831a6866fe9679c2d0c
ByeongSu Do <bsdo@naver.com>
2016-06-28 14:50:56 +0900

    add module export

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 /**
  * Created by dobyeongsu on 2016. 6. 28..
  */
+
+module.exports.Club = require('./Models/Club');
+module.exports.Comment = require('./Models/Comment');
+module.exports.Forum = require('./Models/Forum');
+module.exports.Post = require('./Models/Post');
+module.exports.Search = require('./Models/Search');
+module.exports.Skill = require('./Models/Skill');
+module.exports.Trendbox = require('./Models/Trendbox');
+module.exports.User = require('./Models/User');
+
 module.exports = require('./Models');
