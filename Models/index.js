8d41886966e0383d2b334775e4c056f8043e023b
ByeongSu Do <bsdo@naver.com>
2016-06-28 14:43:02 +0900

    reset

--- Models/index.js
+++ Models/index.js
@@ -13,7 +13,7 @@ let newFiles = files
     return path.parse(file).name
   });
 
-['Club', 'Comment', 'Forum', 'Post', 'Search', 'Skill', 'Trendbox', 'User'].forEach(function (element, index, array) {
+newFiles.forEach(function (element, index, array) {
   models[element] = require(path.resolve(__dirname, element));
 });
 
