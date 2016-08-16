edccb7360ef3a0b44da68a5ee35bca088e0b3593
ByeongSu Do <bsdo@naver.com>
2016-08-16 15:49:50 +0900

    add Collection api

--- Models/Collection/index.js
+++ Models/Collection/index.js
@@ -2,11 +2,35 @@
 const Db = require('trendclear-database').Models;
 
 class Collection {
+  getUserCollections(user) {
+    return user
+      .$relatedQuery('collections')
+      .eager('forum')
+  }
   createCollecion(collectionObj) {
     return Db
       .tc_collections
       .query()
       .insert(collectionObj)
+  }
+  updateCollection(collectionObj) {
+    return Db
+      .tc_collections
+      .query()
+      .insert(collectionObj)
+  }
+  deleteCollection(collectionObj) {
+    return Db
+      .tc_collections
+      .query()
+      .insert(collectionObj)
+  }
+
+  addForum() {
+
+  }
+  removeForum() {
+
   }
 }
 
