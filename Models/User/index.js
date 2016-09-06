ca12b71a8abe8879c63bfd0255a4b2ee124a2137
ByeongSu Do <bsdo@naver.com>
2016-09-07 00:07:26 +0900

    add creator.profile

--- Models/User/index.js
+++ Models/User/index.js
@@ -319,7 +319,7 @@ class User {
         'profile, ' +
         'icon.iconDef, ' +
         'collections.forums, ' +
-        'follow_forums.creator,' +
+        'follow_forums.creator.profile,' +
         'forumCreated' +
         ']')
       .where(userObj)
