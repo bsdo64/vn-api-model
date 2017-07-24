50b6a5d6be7c39b20f868244b355028c2bc294b2
Byeongsu Do <bsdo@naver.com>
2017-07-24 15:30:00 +0900

    update exports

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
  * Created by dobyeongsu on 2016. 6. 28..
  */
 
-module.exports.Db = require('trendclear-database');
 module.exports = require('./Models');
+
+module.exports.Db = require('trendclear-database');
